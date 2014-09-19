da47147611d48b44100437abb71690aca664183b
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2014-09-19 12:58:46 +0200

    store expected counts to compare with other branch

--- Profiling.py
+++ Profiling.py
@@ -1,12 +1,13 @@
 from ForwardBackward import ForwardBackward as FB
 import pickle
 
-tags = set(['LET','LID', 'VZ', 'WW','TSW','ADJ','N','VG','BW','TW','SPEC(e)', 'VNW','VZ+LID','WW+VNW']) 
+tags = set(['LET', 'LID', 'VZ', 'WW', 'TSW', 'ADJ', 'N', 'VG', 'BW', 'TW', 'SPEC(e)', 'VNW', 'VZ+LID', 'WW+VNW'])
 
 hmm = pickle.load(open('statenvertaling.hmm.pickle', 'r'))
 
 s = 'inden beginne schiep godt den hemel , ende de aerde .\n'
 
 training = FB(s, hmm)
-training.compute_all_forward_probabilities()
-training.compute_all_backward_probabilities()
+expected_counts = training.compute_expected_counts()
+
+pickle.dump(expected_counts, open('expected_counts_float.pickle', 'w'))
