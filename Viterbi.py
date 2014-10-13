8553c5d378366f551393ddc8da8aa06006805982
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2014-10-13 10:36:29 +0200

    10x speed up viterbi by removing explicit backpointers

--- Viterbi.py
+++ Viterbi.py
@@ -17,6 +17,7 @@ class Viterbi:
         """
         self.hmm = hmm2
 
+    @profile
     def compute_best_parse(self, sequence):
         """
         Compute the viterbi parse for the sequence.
@@ -28,7 +29,8 @@ class Viterbi:
         N = len(self.hmm.tagIDs)
         length = len(words)
         dynamic_table = numpy.zeros(shape=(length, N, N), dtype=numpy.float64)
-        backpointers = numpy.zeros(shape=dynamic_table.shape, dtype=object)
+
+        backpointers = numpy.zeros(shape=dynamic_table.shape, dtype=numpy.int)
 
         # base case
         dynamic_table[0, -2, :] = self.hmm.transition[-1, -2, :] * self.hmm.emission[:, self.hmm.wordIDs[words[0]]]
@@ -38,33 +40,24 @@ class Viterbi:
             wordID = self.hmm.wordIDs[words[position]]
             probs = numpy.transpose(dynamic_table[position-1, :, :] * self.hmm.transition[:, :, :].transpose(2, 0, 1), (0, 2, 1))
             probs_max = probs.max(axis=2)
-            argmax1 = probs.argmax(axis=2)
+            argmax = probs.argmax(axis=2)
             dynamic_table[position, :, :] = (self.hmm.emission[:, wordID, numpy.newaxis] * probs_max).transpose()
-
-            # set backpointers (could I maybe also do this in the matrix)
-            for tagID1 in xrange(N):
-                argmax_prob = argmax1[tagID1]
-
-                # create the backpointers, I would like to add this to an extra dimension of the current matrix so that
-                # I can do it in one go
-                for tagID2 in xrange(N):
-                    argmax = argmax_prob[tagID2]
-                    backpointer = (position-1, argmax, tagID2)
-                    backpointers[position, tagID2, tagID1] = backpointer
+            backpointers[position, :, :] = argmax
 
         # last tag
-        for tagID1 in xrange(N):
-            for tagID2 in xrange(N):
-                dynamic_table[-1, tagID1, tagID2] *= self.hmm.transition[tagID1, tagID2, -1]
+        dynamic_table[-1, :, :] *= self.hmm.transition[:, :, -1]
 
+        # probability best sequence
         best_prob = dynamic_table[-1, :, :].max()
-        i, j = numpy.unravel_index(dynamic_table[-1, :, :].argmax(), dynamic_table[-1, :, :].shape)
-        # maybe put this in another function
-        best_sequence.append(self.hmm.tagIDs_reversed[j])
-        backpointer = (-1, i, j)
-        for i in xrange(length-1):
+        tagID1, tagID2 = numpy.unravel_index(dynamic_table[-1, :, :].argmax(), dynamic_table[-1, :, :].shape)
+
+        # loop back trough backpointers to find best sequence
+        best_sequence.append(self.hmm.tagIDs_reversed[tagID2])
+        backpointer = (-1, tagID1, tagID2)
+        for position in reversed(xrange(length-1)):
             best_sequence.append(self.hmm.tagIDs_reversed[backpointer[1]])
-            backpointer = backpointers[backpointer]
+            backpointer = (position, backpointers[position+1, tagID2, tagID1], tagID1)
+            tagID1, tagID2 = backpointer[1], backpointer[2]
 
         best_sequence.reverse()
 
