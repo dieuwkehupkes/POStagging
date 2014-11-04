b7feeb8b1f3508fce69ba3ae6678a2994c92d330
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2014-11-04 12:55:26 +0100

    cythonise

--- Profiling.py
+++ Profiling.py
@@ -1,21 +1,13 @@
 import line_profiler as P
-import Test
 import test_accuracy_tagger as t
-import HMMgenerator as gen
-import IterativeTraining
+# mport HMMgenerator as gen
+# import IterativeTraining
 
 profiler = P.LineProfiler()
-profiler.add_module(IterativeTraining)
-profiler.add_module(gen)
-profiler.add_function(gen.HMM2_generator.get_lexicon_counts)
-profiler.add_function(gen.HMM2_generator.get_lexicon_from_file)
 profiler.add_function(t.run)
-profiler.add_function(gen.HMM2_generator.get_lexicon_counts)
-profiler.add_function(gen.HMM2_generator.get_trigrams_from_file)
 
 ftrain = '../../Data/Lassy/lassy.train.spec1'
 feval = '../../Data/Lassy/lassy.dev.spec1'
 
-T = Test.Test()
 profiler.run('t.run(ftrain, feval)')
 profiler.print_stats()
