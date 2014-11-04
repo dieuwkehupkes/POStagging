4caf2f41be2c52aa209c41720eab3fa6e48a3cfd
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2014-11-04 12:51:49 +0100

    cythonise

---
+++ HMM2C.c
@@ -0,0 +1,17 @@
+//#include <iostream>
+#include <string>
+
+using namespace std;
+
+class HMM2  //class representing a second order hidden markov model
+{
+    public:
+        // Here we declare the variables of the class
+        HMM2(float transition[][][], float emission[][][], map tagIDs, map wordIDs)      // constructor
+
+}
+
+HMM2:HMM2(float transition[][][], float emission[][][], map tagIDs, map wordIDs)      // constructor
+{
+    // constructing function
+}
